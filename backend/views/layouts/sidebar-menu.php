e782d69c5c86c9e058537cc38d8704c44f0c623c
forecho <caizhenghai@gmail.com>
2015-07-10 10:37:15 +0800

    first common

---
+++ backend/views/layouts/sidebar-menu.php
@@ -0,0 +1,68 @@
+<?php
+use common\widgets\Menu;
+
+echo Menu::widget(
+    [
+        'options' => [
+            'class' => 'sidebar-menu'
+        ],
+        'items' => [
+            [
+                'label' => Yii::t('app', 'Dashboard'),
+                'url' => Yii::$app->homeUrl,
+                'icon' => 'fa-dashboard',
+                'active' => Yii::$app->request->url === Yii::$app->homeUrl
+            ],
+            [
+                'label' => Yii::t('app', 'Settings'),
+                'url' => ['#'],
+                'icon' => 'fa fa-spinner',
+                'options' => [
+                    'class' => 'treeview',
+                ],
+                'items' => [
+                    [
+                        'label' => Yii::t('app', 'Basic'),
+                        'url' => ['/basic/index'],
+                        'icon' => 'fa fa-user',
+                    ],
+                    [
+                        'label' => Yii::t('app', 'Advanced'),
+                        'url' => ['/advanced/index'],
+                        'icon' => 'fa fa-lock',
+                    ],
+                ],
+            ],
+            [
+                'label' => Yii::t('app', 'RBAC'),
+                'url' => ['#'],
+                'icon' => 'fa fa-cog',
+                'options' => [
+                    'class' => 'treeview',
+                ],
+                'items' => [
+                    [
+                        'label' => Yii::t('app', 'Permissions List'),
+                        'url' => ['/srbac'],
+                        'icon' => 'fa fa-user',
+                    ],
+                    [
+                        'label' => Yii::t('app', 'Role Manage'),
+                        'url' => ['/srbac/role/index'],
+                        'icon' => 'fa fa-user',
+                    ],
+                    [
+                        'label' => Yii::t('app', 'Assign permissions'),
+                        'url' => ['/srbac/default/assign'],
+                        'icon' => 'fa fa-user',
+                    ],
+                    [
+                        'label' => Yii::t('app', 'Assign Child Role'),
+                        'url' => ['/srbac/role/child'],
+                        'icon' => 'fa fa-user',
+                    ],
+                ],
+            ],
+        ]
+    ]
+);
---
+++ common/config/main.php
@@ -0,0 +1,42 @@
+<?php
+return [
+    'name' => 'My Site',
+    'language' => 'zh-CN',
+    'timeZone' => 'Asia/Shanghai',
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            //'enableStrictParsing' => true,
+            'rules' => [
+            ],
+        ],
+        'i18n' => [
+            'translations' => [
+                '*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    //'basePath' => '@app/messages',
+                    //'sourceLanguage' => 'en',
+                    'fileMap' => [
+                        'app' => 'app.php',
+                        'app/error' => 'error.php',
+                    ],
+                ],
+            ],
+        ],
+        'formatter' => [
+            'dateFormat' => 'yyyy-MM-dd',
+            'datetimeFormat' => 'yyyy-MM-dd HH:mm:ss',
+            'decimalSeparator' => ',',
+            'thousandSeparator' => ' ',
+            'currencyCode' => 'CNY',
+        ],
+    ],
+];
